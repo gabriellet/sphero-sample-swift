452cd3f3c54f91be7c45a599ec0760553dd0c736
Gabrielle <gat2118@columbia.edu>
2016-09-20 15:35:37 -0400

    Robot now drives forward 1 unit. Does not drive in other direction.

--- HelloWorldSwift/ViewController.swift
+++ HelloWorldSwift/ViewController.swift
@@ -13,10 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "appWillResignActive:", name: UIApplicationWillResignActiveNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "appDidBecomeActive:", name: UIApplicationDidBecomeActiveNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.appWillResignActive(_:)), name: UIApplicationWillResignActiveNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.appDidBecomeActive(_:)), name: UIApplicationDidBecomeActiveNotification, object: nil)
         
-        RKRobotDiscoveryAgent.sharedAgent().addNotificationObserver(self, selector: "handleRobotStateChangeNotification:")
+        RKRobotDiscoveryAgent.sharedAgent().addNotificationObserver(self, selector: #selector(ViewController.handleRobotStateChangeNotification(_:)))
     }
 
     override func didReceiveMemoryWarning() {
@@ -62,7 +62,11 @@ class ViewController: UIViewController {
                 self.robot = RKConvenienceRobot(robot: noteRobot);
                 
                 connectionLabel.text = noteRobot.name()
+                // togleLED()
+                driveForward()
+                driveBackward()
                 togleLED()
+                
             }
             break
         case .Disconnected:
@@ -85,16 +89,40 @@ class ViewController: UIViewController {
         RKRobotDiscoveryAgent.stopDiscovery()
     }
     
+    func driveForward() {
+        if let robot = self.robot {
+            robot.sendCommand(RKSetHeadingCommand(heading: 0.0))
+            robot.sendCommand(RKRollCommand(heading: 0.0, velocity: 1.0))
+            
+            let delay = Int64(0.5 * Float(NSEC_PER_SEC))
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delay), dispatch_get_main_queue(), { () -> Void in
+                robot.sendCommand(RKRollCommand(heading: 0.0, velocity: 0.0))
+            })
+        }
+    }
+    
+    func driveBackward() {
+        if let robot = self.robot {
+            robot.sendCommand(RKRollCommand(heading: 180.0, velocity: 1.0))
+            
+            let delay = Int64(0.5 * Float(NSEC_PER_SEC))
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delay), dispatch_get_main_queue(), { () -> Void in
+                robot.sendCommand(RKRollCommand(heading: 180.0, velocity: 0.0))
+            })
+        }
+    }
+
+    
     func togleLED() {
         if let robot = self.robot {
             if (ledON) {
                 robot.setLEDWithRed(0.0, green: 0.0, blue: 0.0)
             } else {
-                robot.setLEDWithRed(0.0, green: 0.0, blue: 1.0)
+                robot.setLEDWithRed(0.0, green: 0.8, blue: 0.0)
             }
             ledON = !ledON
             
-            var delay = Int64(0.5 * Float(NSEC_PER_SEC))
+            let delay = Int64(0.5 * Float(NSEC_PER_SEC))
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delay), dispatch_get_main_queue(), { () -> Void in
                 self.togleLED();
             })
