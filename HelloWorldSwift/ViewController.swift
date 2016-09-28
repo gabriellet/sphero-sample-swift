03e67ad576f9d774c9d8d84886a2ddf4e52a981b
Gabrielle <gat2118@columbia.edu>
2016-09-28 16:41:47 -0400

    Layout for four direction driving program done. Buttons not connected correctly.

--- HelloWorldSwift/ViewController.swift
+++ HelloWorldSwift/ViewController.swift
@@ -7,8 +7,22 @@ import UIKit
 class ViewController: UIViewController {
     var robot: RKConvenienceRobot!
     var ledON = false
+    var units = 1.0
+    var dir = "forward"
     
-    @IBOutlet var connectionLabel: UILabel!
+    @IBOutlet weak var connectionLabel: UILabel!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var backwardButton: UIButton!
+    @IBOutlet weak var leftStepper: UIStepper!
+    @IBOutlet weak var rightStepper: UIStepper!
+    @IBOutlet weak var forwardStepper: UIStepper!
+    @IBOutlet weak var backwardStepper: UIStepper!
+    @IBOutlet weak var leftUnitsAmount: UILabel!
+    @IBOutlet weak var rightUnitsAmount: UILabel!
+    @IBOutlet weak var forwardUnitsAmount: UILabel!
+    @IBOutlet weak var backwardUnitsAmount: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +51,32 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func rightStepperPressed(sender: UIStepper) {
+        rightUnitsAmount.text = String(sender.value)
+    }
+    @IBAction func forwardStepperPressed(sender: UIStepper) {
+        forwardUnitsAmount.text = String(sender.value)
+    }
+    @IBAction func backwardStepperPressed(sender: UIStepper) {
+        backwardUnitsAmount.text = String(sender.value)
+    }
+    @IBAction func leftStepperPressed(sender: UIStepper) {
+        leftUnitsAmount.text = String(sender.value)
+    }
+    @IBAction func leftButtonPressed(sender: UIButton) {
+        driveRobot("left", units: leftStepper.value)
+    }
+    @IBAction func rightButtonPressed(sender: UIButton) {
+        driveRobot("right", units: rightStepper.value)
+    }
+    @IBAction func forwardButtonPressed(sender: UIButton) {
+        driveRobot("forward", units: forwardStepper.value)
+    }
+    @IBAction func backwardButtonPressed(sender: UIButton) {
+        driveRobot("backward", units: backwardStepper.value)
+        
+    }
+    
     func appWillResignActive(note: NSNotification) {
         RKRobotDiscoveryAgent.disconnectAll()
         stopDiscovery()
@@ -62,8 +102,7 @@ class ViewController: UIViewController {
                 self.robot = RKConvenienceRobot(robot: noteRobot);
                 
                 connectionLabel.text = noteRobot.name()
-                togleLED()
-                driveForward()
+                toggleRed()
                 //driveBackward()
                 //togleLED()
                 
@@ -89,6 +128,20 @@ class ViewController: UIViewController {
         RKRobotDiscoveryAgent.stopDiscovery()
     }
     
+    func driveRobot(dir: String, units: Double) {
+        var head = 0.0
+        if let robot = self.robot {
+            switch dir {
+                case "forward": head = 0.0
+                case "right": head = 90.0
+                case "backward": head = 180.0
+                case "left": head = 270.0
+                default: break
+            }
+            robot.sendCommand(RKRollCommand(heading: Float(head), velocity: 1.0, andDistance: Float(units)))
+        }
+    }
+    
     func driveForward() {
         if let robot = self.robot {
             robot.sendCommand(RKSetHeadingCommand(heading: 0.0))
@@ -105,16 +158,11 @@ class ViewController: UIViewController {
     func driveBackward() {
         if let robot = self.robot {
             robot.sendCommand(RKRollCommand(heading: 180.0, velocity: 1.0))
-            
-            let delay = Int64(0.5 * Float(NSEC_PER_SEC))
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delay), dispatch_get_main_queue(), { () -> Void in
-                robot.sendCommand(RKRollCommand(heading: 180.0, velocity: 0.0))
-            })
         }
     }
 
     
-    func togleLED() {
+    func toggleGreen() {
         if let robot = self.robot {
             if (ledON) {
                 robot.setLEDWithRed(0.0, green: 0.0, blue: 0.0)
@@ -125,7 +173,7 @@ class ViewController: UIViewController {
             
             let delay = Int64(0.5 * Float(NSEC_PER_SEC))
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delay), dispatch_get_main_queue(), { () -> Void in
-                self.togleLED()
+                self.toggleGreen()
             })
         }
     }
@@ -141,7 +189,7 @@ class ViewController: UIViewController {
             
             let delay = Int64(0.5 * Float(NSEC_PER_SEC))
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delay), dispatch_get_main_queue(), { () -> Void in
-                self.togleLED()
+                self.toggleRed()
             })
         }
     }
